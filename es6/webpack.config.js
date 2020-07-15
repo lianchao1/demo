df5ff64bbbb9c5cbe02a6d49f97376b524fd9307
lianchao <798311258@qq.com>
2020-07-15 20:46:26 +0800

    es6

---
+++ es6/webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+const path = require('path');
+const ImageInlineSizeLimit = 10000; //默认limit是10000了，就是让8kb之内的图片才编码
+
+module.exports = {
+	entry: {
+		main: './src/main.js',
+	},
+	output: {
+		//filename: './dist/[name].js',
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: [/\.js$/],
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}, {
+			test: /\.css$/, //*.global.css->不开启css-loader modules
+			loader: 'style-loader!css-loader'
+		}, {
+			test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+			loader: 'url-loader',
+			options: {
+				limit: ImageInlineSizeLimit,
+				name: 'static/media/[name].[hash:8].[ext]',
+			},
+		}]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			'$': 'jquery',
+			jQuery: 'jquery',
+			'_': 'lodash',
+		})
+	],
+	resolve: {
+		extensions: ['.js', ".json", '.css'],
+		modules: [path.resolve(__dirname, "src"), "node_modules"],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: path.join(__dirname, "page"),//页面目录
+		publicPath: '/dist/',//内存中生成的编译目标目录
+		port: 8081,
+		open: true
+	}
+}
