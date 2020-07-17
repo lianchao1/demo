d5e335a088e829e001fb9b1c56e82cfc982fb9b6
lianchao <798311258@qq.com>
2020-07-17 09:26:32 +0800

    demo

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,1 +1,10 @@
+import {Print} from './one'
+import smallImgSrc from '../images/small.png';
+
+var smallImg = document.createElement("img");
+smallImg.src = smallImgSrc;
+document.body.appendChild(smallImg);
+
 document.write('<h1>Hello World</h1>');
+new Print();
+$("#top").append("<span>444</span>")
--- es6/webpack.config.js
+++ es6/webpack.config.js
@@ -1,11 +1,16 @@
 var webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+	CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const ImageInlineSizeLimit = 10000; //默认limit是10000了，就是让8kb之内的图片才编码
 
 module.exports = {
 	entry: {
 		main: './src/main.js',
+		page1: './src/page1.js',
 	},
 	output: {
 		//filename: './dist/[name].js',
@@ -35,18 +40,44 @@ module.exports = {
 			'$': 'jquery',
 			jQuery: 'jquery',
 			'_': 'lodash',
-		}),		
-		new CleanWebpackPlugin()
+		}),
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [{
+				from: path.join(__dirname, 'src/origin.js')
+			}]
+		}),
+		new HtmlWebpackPlugin({
+			chunks: ['main'], //添加引入的js,也就是entry中的key
+			filename: 'main.html',
+			minify: {
+				collapseWhitespace: false //折叠空白区域 也就是压缩代码
+			},
+			hash: true,
+			title: 'main',
+			template: path.join(__dirname, './page/main.html') //模板地址
+		}),
+		new HtmlWebpackPlugin({
+			chunks: ['page1'], //添加引入的js,也就是entry中的key
+			filename: 'page1.html',
+			minify: {
+				collapseWhitespace: false //折叠空白区域 也就是压缩代码
+			},
+			hash: true,
+			title: 'page',
+			template: path.join(__dirname, './page/page1.html') //模板地址
+		})
 	],
 	resolve: {
 		extensions: ['.js', ".json", '.css'],
 		modules: [path.resolve(__dirname, "src"), "node_modules"],
 	},
-	devtool: 'eval-source-map',//eval-source-map
+	devtool: 'eval-source-map', //eval-source-map
 	devServer: {
-		contentBase: path.join(__dirname, "page"), //页面目录
-		publicPath: '/dist/', //内存中生成的编译目标目录
+		//contentBase: path.join(__dirname, "dist"), //页面目录
+		//publicPath: '/dist/', //内存中生成的编译目标目录
 		port: 8081,
-		open: true
+		open: true,
+		openPage: 'main.html'
 	}
 }
