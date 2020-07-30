21bcf5a5e769819db0395f3af8c39f24564cc5b3
lianchao <798311258@qq.com>
2020-07-30 10:49:30 +0800

    es6

--- es6/src/page1.js
+++ es6/src/page1.js
@@ -1,11 +1,11 @@
 import {Print} from './print'
 import bigImgSrc from '../images/big.png';
-
+import 'isomorphic-fetch'
 var bigImg = document.createElement("img");
 bigImg.src = bigImgSrc;
 document.body.appendChild(bigImg);
 
-document.write('<h1>Hello World</h1>');
+fetch('http://localhost:8081/es6/page2.html').then(r=>r.text()).then( t=>$('body').append(t));
 new Print();
 xbtx.b=1;
 setInterval(function(){console.log(xbtx.b)},5000)
